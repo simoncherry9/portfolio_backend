5e3c8a6fbf1511c1ea4d37f6efa8f564420fb9fc
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-24 12:05:07 -0300

    Modificacion en el mensaje del mail

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -19,7 +19,7 @@ class UsersController {
                 role: userRole,
             });
 
-            const token = jwt.sign({ role: user.role }, process.env.SECRET_KEY, { algorithm: 'HS256' });
+            const token = jwt.sign({ role: User.role }, process.env.SECRET_KEY, { algorithm: 'HS256' });
 
             const transporter = nodemailer.createTransport({
                 host: 'smtp.gmail.com',
@@ -32,17 +32,31 @@ class UsersController {
             });
 
             const mailOptions = {
-                from: process.env.SMTP_EMAIL,
+                from: "simoncherry9@gmail.com",
                 to: email,
                 subject: 'Bienvenido a nuestra página web',
-                text: `¡Hola ${username}!\n\nBienvenido a nuestra web. Esperamos que disfrutes de la experiencia.\n\nSaludos. \n\nSimón Cherry`,
+                html:
+                    `
+                    <h1>¡Hola ${username}!</h1>
+                    <p>Bienvenido a nuestra web. Este e-mail es para agradecerte tu apoyo al registrarte en mi portfolio.</p>
+                    <p>Esperamos que disfrutes de la experiencia.</p>
+                    <p>Podrás iniciar sesión a traves del siguiente link</p>
+                    <ul>
+                    <li><a href="https://ejemplo.com">Iniciar Sesión</a></li>
+                    </ul>
+                    <h3> Los datos de tu cuenta son:</h3>
+                    <p> <b> Email: </b> ${email} </p>
+                    <p> <b> Password: </b> ${password} </p>
+                    <p>Saludos,</p>
+                    <p>Simón Cherry</p>
+                    `,
             };
 
             transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
                     console.log('> USUARIO > CREAR > ENVIAR EMAIL -----> ERROR', error);
                 } else {
-                    console.log('> USUARIO > CREAR > ENVIAR EMAIL -----> EMAIL ENVIADO A ID:', userId);
+                    console.log('> USUARIO > CREAR > ENVIAR EMAIL -----> EMAIL ENVIADO A ID:', userId, info);
                 }
             });
 
