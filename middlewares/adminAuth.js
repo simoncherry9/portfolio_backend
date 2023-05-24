ba4e58f32fb177cb57f59e003ffa6566d6716943
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-24 10:51:13 -0300

    creacion de admin auth para no repetir cdigo

---
+++ middlewares/adminAuth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+function verifyAdminToken(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        console.log("> EXPERIENCIA LABORAL > CREAR -----> ERROR -----> NO SE DETECTO TOKEN ----->", err);
+        return res.status(401).json({ error: 'Token de autenticación no proporcionado' });
+    }
+
+    const tokenWithoutScheme = token.split(' ')[1];
+
+    jwt.verify(tokenWithoutScheme, process.env.SECRET_KEY, { algorithms: ['HS256'] }, (err, decoded) => {
+        if (err) {
+            console.log("> EXPERIENCIA LABORAL > CREAR -----> ERROR -----> TOKEN DE AUTENTICACION IVALIDO ----->", err);
+            return res.status(401).json({ error: 'Token de autenticación inválido' });
+        }
+
+        if (decoded.role !== 'admin') {
+            console.log("> EXPERIENCIA LABORAL > CREAR -----> ERROR -----> ACCESO NO AUTORIZADO ----->", err);
+            return res.status(403).json({ error: 'Acceso no autorizado' });
+        }
+
+        req.user = decoded;
+        next();
+    });
+}
+
+module.exports = { verifyAdminToken };
