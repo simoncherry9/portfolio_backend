1ff68c65da4e1a9087f7a5229c2afa7c922de261
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-05-25 11:55:16 -0300

    Se ajusto la logica de form para q avise por mail

--- src/controllers/formularios.controller.js
+++ src/controllers/formularios.controller.js
@@ -1,4 +1,5 @@
 const pool = require('../../DB');
+const nodemailer = require('nodemailer');
 
 // Método para crear un formulario
 async function createForm(req, res) {
@@ -11,6 +12,62 @@ async function createForm(req, res) {
         await pool.query(query, values);
         console.log(` > FORMULARIO > CREAR-----> FORMULARIO CREADO`)
         res.json("Formulario creado con exito");
+
+        const transporter = nodemailer.createTransport({
+            // Configura aquí los detalles del servidor de correo saliente (SMTP)
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'simoncherry9@gmail.com',
+                pass: 'qielbknpzzdbunxr',
+            },
+        });
+
+        const mailOptions = {
+            from: 'simoncherry9@gmail.com',
+            to: 'simoncherry297@gmail.com',
+            subject: 'Nuevo formulario recibido',
+            html:
+                `
+                    <h1>¡Hola Simón!</h1>
+                    <p>Acaban de dejarte un nuevo formulario de contacto en tu portfolio</p>
+                    <p> El formulario lo dejó <strong>${nombre}</strong> con el siguiente mensaje: <h3><strong>${mensaje}<strong></h3></p>
+                    <h3> Si deseas puedes responderle a traves del siguiente correo electrónico: ${email}</h3>
+                    <p>Saludos,</p>
+                    <p>Envio automático.</p>
+                    `,
+        };
+        const mailOptions2 = {
+            from: 'simoncherry9@gmail.com',
+            to: email,
+            subject: 'Mensaje enviado',
+            html:
+                `
+                    <h1>¡Hola ${nombre}!</h1>
+                    <p>Ya le hemos enviado tu formulario de contacto a Simón</p>
+                    <p>El te responderá lo más pronto posible, si deseas puedes enviarle un email al siguiente correo: simoncherry297@gmail.com</p>
+                    <p>Saludos,</p>
+                    <p>Envio automático.</p>
+                    `,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error("> FORMULARIO > CREAR -----> ERROR AL ENVIAR CORREO ELECTRÓNICO", error);
+            } else {
+                console.log("> FORMULARIO > CREAR -----> CORREO ELECTRÓNICO ENVIADO", info);
+            }
+        });
+
+        transporter.sendMail(mailOptions2, (error, info) => {
+            if (error) {
+                console.error("> FORMULARIO > CREAR -----> ERROR AL ENVIAR CORREO ELECTRÓNICO", error);
+            } else {
+                console.log("> FORMULARIO > CREAR -----> CORREO ELECTRÓNICO ENVIADO", info);
+            }
+        });
+
     } catch (error) {
         console.error('> FORMULARIO > CREAR -----> ERROR -----> ', error);
         res.status(500).json({ error: 'Error interno al crear el formulario' });
